cce2944657a18b6a9b8cbafef42628759c78d10f
Rolando HR <rolando167@hotmail.com>
2021-09-07 23:40:00 -0500

    cas

---
+++ src/users/user.entity.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+
+@Crud({
+	model:{
+		type: User
+	}
+})
+
+@Controller('users')
+export class UsersController implements CrudController<User> {
+
+	constructor(public service: UsersService){}
+	// @Get()
+	// getUsers(){
+	//     // http://localhost:3000/users
+	//     return 'the users';
+	// }
+}
